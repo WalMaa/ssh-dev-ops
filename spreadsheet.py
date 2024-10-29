d2adefff6c3b806411b9e72230258f156ffd07f1
WalMaa <98175031+WalMaa@users.noreply.github.com>
2024-10-29 17:23:03 +0200

    evaluate green

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,8 +1,8 @@
+
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
@@ -10,5 +10,6 @@ class SpreadSheet:
     def get(self, cell: str) -> str:
         return self._cells.get(cell, '')
 
-    def evaluate(self, cell: str) -> int | str:
-        pass
+    def evaluate(self, cell: str) -> int:
+        return int(self._cells[cell])
+
